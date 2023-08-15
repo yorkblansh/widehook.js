c01eb447ff32bf4f9e297f2d535f2f4439a93ab4
Oleg Chepelev <yorkblansh@yandex.com>
2023-08-15 15:11:19 +0300

    upd app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './app-style.css'
-import { Hello } from '../dist'
+import { useState } from "react"
+import logo from "./logo.svg"
+import "./app-style.css"
+import { Hello } from "../dist/main"
 
+function App() {
+	const [count, setCount] = useState(0)
 
-function App () {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Hello />
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
-    </div>
-  )
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<Hello />
+				<p>
+					<button type="button" onClick={() => setCount((count) => count + 1)}>
+						count is: {count}
+					</button>
+				</p>
+			</header>
+		</div>
+	)
 }
 
 export default App
