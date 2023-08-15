b5ad44379c6b3747b13d74b07f0415960e36ff17
Oleg Chepelev <yorkblansh@yandex.com>
2023-08-15 21:37:35 +0300

    upd vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { resolve } from "node:path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tsConfigPaths from "vite-tsconfig-paths"
-import dts from "vite-plugin-dts"
-import { EsLinter, linterPlugin } from "vite-plugin-linter"
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
@@ -17,11 +17,11 @@ export default defineConfig((configEnv) => ({
 		dts({
 			insertTypesEntry: true,
 
-			include: ["lib/widehook.tsx"],
+			include: ['lib/widehook.tsx'],
 
 			beforeWriteFile: (filePath, content) => {
 				return {
-					filePath: filePath.replace("/lib", ""),
+					filePath: filePath.replace('/lib', ''),
 					content,
 				}
 			},
@@ -29,16 +29,17 @@ export default defineConfig((configEnv) => ({
 	],
 
 	build: {
+		minify: true,
 		lib: {
-			formats: ["es", "cjs"],
-			entry: resolve("lib", "widehook.tsx"),
-			name: "ReactFeatureFlag",
+			formats: ['es', 'cjs'],
+			entry: resolve('lib', 'widehook.tsx'),
+			name: 'ReactFeatureFlag',
 			fileName: (format, entryName) => {
 				return `widehook.${format}.js`
 			},
 		},
 		rollupOptions: {
-			external: ["react"],
+			external: ['react'],
 		},
 	},
 }))
