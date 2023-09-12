db5f4895d9a5ae6309356ee4a4b455c2afc228c7
Oleg Chepelev <yorkblansh@yandex.com>
2023-09-12 21:49:57 +0300

    upd examples

--- example/hooks/useText.ts
+++ example/hooks/useText.ts
@@ -1,23 +1,43 @@
 import { createWideHook } from '@widehook'
-import { useNumber } from './useNumber'
+// import { useNumber } from './useNumber'
 
 type Text = 'One Text' | 'Another Text' | 'Completely Different Text'
 
+export const useNumber = createWideHook({
+	init: 7,
+})
+
 export const useText = createWideHook({
-	init: 'Init Text' as Text,
-	on: (text, setText, here) => {
+	//            ^?
+
+	init: 'text' as Text,
+	on: (message, setMessage, here) => {
+		//    ^?
+
 		const [number, setNumber, inNumber] = here.takeOtherStateByHook(useNumber)
+		//        ^?
 
-		// setTimeout(() => {
-		// 	setText(text + '1')
-		// }, 1500)
-		// if (text === 'Completely Different Text') setNumber(number + 1)
-
-		console.table({
-			text,
-			textPrevState: here.prevState(),
-			number: number,
-			numberPrevState: inNumber.prevState(),
-		})
+		console.log({ prevMessage: inNumber.prevState() })
 	},
 })
+
+export const Component = () => {
+	const [text, setText] = useText()
+	//       ^?
+
+	return <div>{text}</div>
+}
+
+// const [number, setNumber, inNumber] = here.takeOtherStateByHook(useNumber)
+
+// 		// setTimeout(() => {
+// 		// 	setText(text + '1')
+// 		// }, 1500)
+// 		// if (text === 'Completely Different Text') setNumber(number + 1)
+
+// 		console.table({
+// 			text,
+// 			textPrevState: here.prevState(),
+// 			number: number,
+// 			numberPrevState: inNumber.prevState(),
+// 		})
