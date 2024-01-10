58bc5a503e921cd57e2d209a5f5bbd35ec11ab1d
Oleg Chepelev <yorkblansh@yandex.com>
2024-01-10 14:45:00 +0000

    u

--- src/createWideHook.ts
+++ src/createWideHook.ts
@@ -6,7 +6,7 @@ import type { ActionCallback, Scope } from './types/ActionCallback'
 import { capitalize } from './utils/capitalize'
 import type { ExtraSettings } from './types/ExtraSettings'
 
-const y = 6787687689879879879879
+const y = 6787687689879879879879000
 
 export function createWideHook<
 	State,
